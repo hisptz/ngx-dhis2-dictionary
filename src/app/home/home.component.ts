ff9b9505b34034195c0de877d1a948516f55be5d
josephatJ <josephatjulius24@gmail.com>
2019-04-28 12:30:22 +0300

    feat: add shared link button to enable sharing selected indicators and other metadata

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import * as _ from 'lodash';
 import { Identifiers } from '@angular/compiler';
+import { NgxDhis2HttpClientService } from '@iapps/ngx-dhis2-http-client';
 
 @Component({
   selector: 'app-home',
@@ -12,12 +13,15 @@ export class HomeComponent implements OnInit {
 
   selectedItem: string;
   metadataIdentifiers: any;
+  systemSettings: any;
   metadataIdentifiersArr: any[] = ['BfMAe6Itzgt', 'ulgL07PF8rq','sB79w2hiLp8','vDdRoZYybP2', 'Kswd1r4qWLh', 'O8Kuzjsx2Zm','fbfJHSPpUD','xsRrGKBoLCm'];
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(private router: Router, private route: ActivatedRoute, private httpClient: NgxDhis2HttpClientService) {}
 
   ngOnInit() {
+    this.httpClient.get('system/info').subscribe((systemInfoSettings) => {
+      this.systemSettings = systemInfoSettings;
+    })
     this.route.params.forEach((params: Params) => {
-      console.log(params['selected'])
       if (params['selected'] != undefined) {
         if (params['selected'] == 'all' && !params['ids']) {
           this.metadataIdentifiers = [];
@@ -29,12 +33,15 @@ export class HomeComponent implements OnInit {
           params['ids'].split(',').forEach((param) => {
             identifiers.push(param);
           })
-          identifiers.push(this.selectedItem);
+          if (this.selectedItem != 'all') {
+            identifiers.push(this.selectedItem);
+          }
           this.metadataIdentifiers = _.uniq(identifiers);
           this.router.navigate(['dictionary/' + _.uniq(identifiers).join(',') + '/selected/' + this.selectedItem])
         }
       } else {
         this.metadataIdentifiers = this.metadataIdentifiersArr;
+        this.selectedItem = this.metadataIdentifiers[0];
         this.router.navigate(['dictionary/' + _.uniq(this.metadataIdentifiers).join(',') + '/selected/' + this.metadataIdentifiers[0]])
       }
     })
@@ -51,6 +58,7 @@ export class HomeComponent implements OnInit {
           }
         })
         this.metadataIdentifiers = identifiers;
+        this.selectedItem = listOfItemsObj.selected;
         this.router.navigate(['dictionary/' + _.uniq(identifiers).join(',') + '/selected/' + listOfItemsObj.selected])
       } else {
         this.router.navigate(['dictionary/all'])
@@ -66,8 +74,27 @@ export class HomeComponent implements OnInit {
         identifiers.push(listOfItemsObj.selected)
       }
       this.metadataIdentifiers = _.uniq(identifiers);
+      this.selectedItem = listOfItemsObj.selected;
       this.router.navigate(['dictionary/' + _.uniq(identifiers).join(',') + '/selected/' + listOfItemsObj.selected])
     }
   }
 
+  getSharedLink(identifiers) {
+    if (this.systemSettings) {
+      let selBox = document.createElement('textarea');
+      selBox.style.position = 'fixed';
+      selBox.style.left = '0';
+      selBox.style.top = '0';
+      selBox.style.opacity = '0';
+      selBox.value = this.systemSettings.keyRemoteInstanceUrl + '/api/apps/Indicator-Dictionary/index.html#/dictionary/' + _.uniq(identifiers).join(',') + '/selected/' + this.selectedItem;
+      document.body.appendChild(selBox);
+      selBox.focus();
+      selBox.select();
+      document.execCommand('copy');
+      document.body.removeChild(selBox);
+      let messagePart = document.getElementById('copied-message')
+      setTimeout(function(){ messagePart.style.display = 'inline-block'; messagePart.style.marginBottom = '-20px';}, 100);
+      setTimeout(function(){ messagePart.style.display = 'none'; }, 1500);
+    }
+  }
 }
