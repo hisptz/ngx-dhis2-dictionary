dc01a0495a056e051a0ca0d87820aeb47dad1b72
josephatJ <josephatjulius24@gmail.com>
2020-01-07 18:38:11 +0300

    feat: Check for empty word in the searching terms

--- projects/ngx-dhis2-dictionary/src/lib/pipes/filter-by-search-input.pipe.ts
+++ projects/ngx-dhis2-dictionary/src/lib/pipes/filter-by-search-input.pipe.ts
@@ -16,49 +16,38 @@ export class FilterBySearchInputPipe implements PipeTransform {
         let newIndicators = [];
 
         splittedText.split(" ").forEach(partOfSearchingText => {
-          _.map(newIndicators, formattedIndicator => {
-            if (
-              formattedIndicator.name
-                .toLowerCase()
-                .indexOf(partOfSearchingText.toLowerCase()) > -1
-            ) {
-              newIndicators[
-                _.findIndex(newIndicators, {
-                  id: formattedIndicator.id
-                })
-              ]["priority"] += 1;
-            }
-          });
+          if (partOfSearchingText != "") {
+            _.map(newIndicators, formattedIndicator => {
+              if (
+                formattedIndicator.name
+                  .toLowerCase()
+                  .indexOf(partOfSearchingText.toLowerCase()) > -1
+              ) {
+                newIndicators[
+                  _.findIndex(newIndicators, {
+                    id: formattedIndicator.id
+                  })
+                ]["priority"] += 1;
+              }
+            });
 
-          _.map(indicators, indicator => {
-            if (
-              indicator.name
-                .toLowerCase()
-                .indexOf(partOfSearchingText.toLowerCase()) > -1 &&
-              _.findIndex(newIndicators, {
-                id: indicator.id
-              }) == -1
-            ) {
-              indicator["priority"] = 1;
-              newIndicators.push(indicator);
-            }
-          });
+            _.map(indicators, indicator => {
+              if (
+                indicator.name
+                  .toLowerCase()
+                  .indexOf(partOfSearchingText.toLowerCase()) > -1 &&
+                _.findIndex(newIndicators, {
+                  id: indicator.id
+                }) == -1
+              ) {
+                indicator["priority"] = 1;
+                newIndicators.push(indicator);
+              }
+            });
+          }
         });
 
         indicators = _.orderBy(newIndicators, ["priority"], ["desc"]);
-        // return indicators.filter(indicator => {
-        //   let foundIndicatorMatchingSearchingInput = true;
-        //   splittedText.split(" ").forEach(partOfSearchingText => {
-        //     if (
-        //       indicator.name
-        //         .toLowerCase()
-        //         .indexOf(partOfSearchingText.toLowerCase()) === -1
-        //     ) {
-        //       foundIndicatorMatchingSearchingInput = false;
-        //     }
-        //   });
-        //   return foundIndicatorMatchingSearchingInput;
-        // });
       }
     }
     return indicators;
